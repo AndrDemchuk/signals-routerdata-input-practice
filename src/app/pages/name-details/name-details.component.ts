4c49dc7b183be81af376a97d4629a28d84acb71b
AndrDemchuk <xdemchuk@gmail.com>
2026-05-23 21:13:06 +0300

    task solution

---
+++ src/app/pages/name-details/name-details.component.ts
@@ -0,0 +1,24 @@
+import { Component, computed, input } from '@angular/core';
+
+@Component({
+  selector: 'app-name-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './name-details.component.html',
+  styleUrl: './name-details.component.scss'
+})
+export default class NameDetailsComponent {
+
+  readonly name = input.required();
+
+  readonly upperCaseName = computed(() => String(this.name()).toUpperCase());
+  readonly NameLength = computed(() => String(this.name()).length);
+
+
+  // Create a 'name' signal here, which receives the name parameter from the router
+
+  // Create a computed signal 'upperCaseName' which returns the name in uppercase
+
+  // Create a computed signal 'NameLength' which returns the length of the name
+
+}
